38e4345bbb16268494750d50f50a4b8577f9f9b1
yasminmessycodes <84807559+yasmin-hashem24@users.noreply.github.com>
2023-07-14 22:36:08 +0300

    fixed async and initialized on declaration and fixed namespace

--- DealingWithCookies/DealingWithCookies/Pages/StarredFeeds.cshtml.cs
+++ DealingWithCookies/DealingWithCookies/Pages/StarredFeeds.cshtml.cs
@@ -1,22 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Text.Json;
-namespace DealingWithCookies.Pages
+namespace DealingWithCookies.Pages; 
+
+public class StarredFeedsModel : PageModel
 {
-    public class StarredFeedsModel : PageModel
+    public List<Report> StarredReports { get; set; }
+    public void OnGet()
     {
-        public List<Report> StarredReports { get; set; }
-        public void OnGet()
-        {
-            var starredItems = Request.Cookies["starredCookie"] != null ?
-               JsonSerializer.Deserialize<Dictionary<string, Report>>(Request.Cookies["starredCookie"]) :
-               new Dictionary<string, Report>();
+        var starredItems = Request.Cookies["starredCookie"] != null ?
+           JsonSerializer.Deserialize<Dictionary<string, Report>>(Request.Cookies["starredCookie"]) :
+           new Dictionary<string, Report>();
 
-            var reports = JsonSerializer.Deserialize<List<Report>>(System.IO.File.ReadAllText("starredCookie.json"));
+        var reports = JsonSerializer.Deserialize<List<Report>>(System.IO.File.ReadAllText("starredCookie.json"));
 
-            StarredReports = reports.Where(r => starredItems.ContainsKey(r.PubDate))
-                         .OrderByDescending(r => r.PubDate)
-                         .ToList();
-        }
+        StarredReports = reports.Where(r => starredItems.ContainsKey(r.PubDate))
+                     .OrderByDescending(r => r.PubDate)
+                     .ToList();
     }
 }
