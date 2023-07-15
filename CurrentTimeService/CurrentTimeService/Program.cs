c398841c9d6ea686634654d6335f3b56e1b86b1a
yasminmessycodes <84807559+yasmin-hashem24@users.noreply.github.com>
2023-07-16 00:56:06 +0300

    indentation

--- CurrentTimeService/CurrentTimeService/Program.cs
+++ CurrentTimeService/CurrentTimeService/Program.cs
@@ -1,13 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,12 +13,10 @@ app.UseHttpsRedirection();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-
 app.MapGet("/Current-Time-Servies", () =>
 {
-    var currentTime = DateTime.Now.ToString();
-    return Results.Text(currentTime, "text/html");
+    var CurrentTime = DateTime.Now.ToString();
+    return Results.Text(CurrentTime, "text/html");
 });
-
 
 app.Run();
