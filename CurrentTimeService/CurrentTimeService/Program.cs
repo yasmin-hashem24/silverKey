e4b073e13f64f45c16e6ae6158b048033e4578da
yasminmessycodes <84807559+yasmin-hashem24@users.noreply.github.com>
2023-07-16 00:54:04 +0300

    Current Time Service Code

---
+++ CurrentTimeService/CurrentTimeService/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+
+app.MapGet("/Current-Time-Servies", () =>
+{
+    var currentTime = DateTime.Now.ToString();
+    return Results.Text(currentTime, "text/html");
+});
+
+
+app.Run();
