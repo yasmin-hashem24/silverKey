db7a72117d2b15dc1911610a70a5c90d4d920d78
yasminmessycodes <84807559+yasmin-hashem24@users.noreply.github.com>
2023-07-17 16:42:47 +0300

    fixed aythentication

--- CookiesAuthentication/CookiesAuthentication/Pages/Index.cshtml.cs
+++ CookiesAuthentication/CookiesAuthentication/Pages/Index.cshtml.cs
@@ -4,61 +4,54 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
 using System.Security.Claims;
 
-namespace CookiesAuthentication.Pages;
-
-public class IndexModel : PageModel
+namespace CookiesAuthentication.Pages
 {
-
-    [BindProperty]
-    public string UserName { get; set;}
-
-    [BindProperty]
-    public string Password { get; set; }
-
-    public Boolean inCorrectly = false;
-
-    public async Task<IActionResult> OnPost()
+    public class IndexModel : PageModel
     {
+        [BindProperty]
+        public string UserName { get; set; }
 
-        if (ModelState.IsValid)
+        [BindProperty]
+        public string Password { get; set; }
+
+        public async Task<IActionResult> OnPost()
         {
-            if (this.UserName == "intern" &&  this.Password == "summer 2023 july")
+            if (ModelState.IsValid)
             {
-                var claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Name, this.UserName),
-            };
-
-                var claimsIdentity = new ClaimsIdentity(
-                claims, CookieAuthenticationDefaults.AuthenticationScheme);
-
-                var authProperties = new AuthenticationProperties
+                if (this.UserName == "intern" && this.Password == "summer 2023 july")
                 {
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, this.UserName),
+                    };
 
-                    ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
-                    IsPersistent = true,
+                    var claimsIdentity = new ClaimsIdentity(
+                        claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-                };
+                    var authProperties = new AuthenticationProperties
+                    {
+                        ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                        IsPersistent = true,
+                    };
 
-                await HttpContext.SignInAsync(
-                    CookieAuthenticationDefaults.AuthenticationScheme,
-                    new ClaimsPrincipal(claimsIdentity),
-                    authProperties);
+                    await HttpContext.SignInAsync(
+                        CookieAuthenticationDefaults.AuthenticationScheme,
+                        new ClaimsPrincipal(claimsIdentity),
+                        authProperties);
 
-                inCorrectly = true;
+                    return RedirectToPage("/Index");
+                }
             }
-           
-        };
 
-        return Page();
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostLogoutAsync()
+        {
+            await HttpContext.SignOutAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToPage("/Index");
+        }
     }
-
-    public async Task<IActionResult> OnPostLogoutAsync()
-    {
-        await HttpContext.SignOutAsync(
-            CookieAuthenticationDefaults.AuthenticationScheme);
-
-        return RedirectToPage("/Index");
-    }
-
 }
