8d739420ebe4ca46b8ae9a8e4a6ff10426570aef
yasminmessycodes <84807559+yasmin-hashem24@users.noreply.github.com>
2023-08-10 07:05:28 +0300

    code migration

---
+++ ContactsBlazor/ContactsBlazor/Client/InputContact.cs
@@ -0,0 +1,17 @@
+﻿namespace ContactsBlazor.Client
+{
+
+    public class InputContact
+    {
+        public string FirstName { get; set; } = " ";
+        public string LastName { get; set; } = " ";
+        public string Email { get; set; } = " ";
+        public string Title { get; set; } = " ";
+        public string Description { get; set; } = " ";
+        public string DateOfBirth { get; set; } = " ";
+        public string UserName { get; set; } = " ";
+        public string Password { get; set; } = " ";
+        public string Role { get; set; } = " ";
+        public bool MarriageStatus { get; set; } = false;
+    }
+}
