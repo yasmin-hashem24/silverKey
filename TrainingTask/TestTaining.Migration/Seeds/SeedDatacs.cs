145f85297af857525f92d51ce6da69b24a95b2c0
yasminmessycodes <84807559+yasmin-hashem24@users.noreply.github.com>
2023-10-13 23:43:04 +0300

    LLGB

---
+++ TrainingTask/TestTaining.Migration/Seeds/SeedDatacs.cs
@@ -0,0 +1,12 @@
+﻿/*using FluentMigrator.Runner.Initialization;
+
+
+namespace Customers.Migration._100.Seeds;
+public class SeedData : ISeed
+{
+    public void Seed()
+    {
+        Insert.IntoTable("Students").Row(new { Name = "Yasmin Hashem" });
+    }
+}
+*/
