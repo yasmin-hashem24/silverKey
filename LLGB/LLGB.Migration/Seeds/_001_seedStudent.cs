8f13980faff4365b31705dde3aaff341f6031112
yasminmessycodes <84807559+yasmin-hashem24@users.noreply.github.com>
2023-10-14 19:30:55 +0300

    Adding New Project and removing the ruined structured one

---
+++ LLGB/LLGB.Migration/Seeds/_001_seedStudent.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+namespace LLGB.Migration.Seeds
+{
+    [Migration(2)]
+    public class _001_seedStudent : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Insert.IntoTable("student").Row(new { Email = "yasmin.hashem201.com", Name = "Yasmina" });
+           
+        }
+
+    }
+
+
+}
+
